80d0b6a4997cac35d483644cdb02de778279039e
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2016-01-15 10:53:54 -0500

    First commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,9 @@
+/**
+ * @author Jayesh Chandrapal.
+ * @date 1/15/16
+ */
+public class MainApp {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
